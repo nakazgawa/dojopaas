78b0bbc022711bfc7ff413a430a0dc1da6f64477
AnaTofuZ <anatofuz@gmail.com>
2019-05-23 15:27:05 +0900

    added methods and write comments at sakura_server.rb
    sakura_server.rbはまだ仕様が固まってないこともあるので、メモ代わりにコメントを追加した
    いくつか使いそうなメソッドを仮で書いた

--- ruby_scripts/sakura_server.rb
+++ ruby_scripts/sakura_server.rb
@@ -1,5 +1,8 @@
+#!/usr/bin/env ruby
+
 class SakuraServer
 
+  # jsのserver.createで使っているフィールドを参考
   def initialize(zone:0, plan:nil, packetfilterid:nil, name:nil, description:nil, tags:nil, pubkey:nil, disk:nil, resolve:nil, notes:nil)
       @zone           = zone
       @plan           = plan
@@ -12,9 +15,27 @@ class SakuraServer
       @resolve        = resolve
       @notes          = notes
   end
+
+  # server.createに対応
+  # 引数があると、オブジェクトの状態を変えつつそちらを使う
+  def create(server_params)
+  end
+
+  private
+
+  # createとdestroyで独自に引数を取れるようにしておく
+  def init_instance_variable(params)
+
+  end
 end
 
 
+# main
+
+# sakuraserverのインスタンスを作成し、createする流れ
+# おそらくclassの部分は切り貼りし、mainは実際には使わないのでテスト実装
+
 sakura =  SakuraServer.new(zone: 5, tags:"test")
 
+#sakura.create() <- createする際はこれを呼び出す
 
