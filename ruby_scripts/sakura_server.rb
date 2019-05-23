1f62972fb8f8a23dd96ede4582411e61aae3eb4c
AnaTofuZ <anatofuz@gmail.com>
2019-05-23 20:58:08 +0900

    インデントを修正
    半角スペース2に変更した

--- ruby_scripts/sakura_server.rb
+++ ruby_scripts/sakura_server.rb
@@ -6,23 +6,24 @@ class SakuraServer
 
   # jsのserver.createで使っているフィールドを参考
   def initialize(zone:0, plan:nil, packetfilterid:nil, name:nil, description:nil, tags:nil, pubkey:nil, disk:nil, resolve:nil, notes:nil)
-      @zone           = zone
-      @plan           = plan
-      @packetfilterid = packetfilterid
-      @name           = name
-      @description    = description
-      @tags           = tags
-      @pubkey         = pubkey
-      @disk           = disk
-      @resolve        = resolve
-      @notes          = notes
+    @zone           = zone
+    @plan           = plan
+    @packetfilterid = packetfilterid
+    @name           = name
+    @description    = description
+    @tags           = tags
+    @pubkey         = pubkey
+    @disk           = disk
+    @resolve        = resolve
+    @notes          = notes
 
-      @client = HTTPClient.new
+    @client = HTTPClient.new
   end
 
   # server.createに対応
   # 引数があると、オブジェクトの状態を変えつつそちらを使う
   def create(server_params)
+
   end
 
   private
@@ -30,14 +31,14 @@ class SakuraServer
   # createとdestroyで独自に引数を取れるようにしておく
   def init_instance_variable(params)
 
-      #インスタンス作成
-      
-      send_request('get', '', '')
+    #インスタンス作成
+    
+    send_request('get', '', '')
   end
 
 
   def send_request(http_method,endpoint,query)
-      @client.send(lc(http_method),endpoint,query)
+    @client.send(lc(http_method),endpoint,query)
   end
   #def check_parameters
   #end
